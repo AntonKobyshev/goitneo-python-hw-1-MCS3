803b3488791d8fccc6730a2b81b9d706b6402e6f
Anton Kobyshev <antonkobyshev@gmail.com>
2023-10-13 11:33:33 +0300

    update

--- birthday_assistant.py
+++ birthday_assistant.py
@@ -1,8 +1,10 @@
+import json
 from datetime import datetime
 from collections import defaultdict
 
 
-def get_birthdays_per_week(users):
+def get_birthdays_per_week(users_data):
+
     birthdays_per_week = defaultdict(list)
     days_of_week = {
         0: 'Monday', 1: 'Tuesday', 2: 'Wednesday',
@@ -11,7 +13,7 @@ def get_birthdays_per_week(users):
 
     today = datetime.today().date()
 
-    for user in users:
+    for user in users_data:
         name = user["name"]
         birthday = user["birthday"].date()
         birthday_this_year = birthday.replace(year=today.year)
@@ -32,23 +34,11 @@ def get_birthdays_per_week(users):
         print(f"{day}: {', '.join(users_list)}")
 
 
-users = [
-    {"name": "John Smith", "birthday": datetime(1990, 5, 15)},
-    {"name": "Emily Davis", "birthday": datetime(1985, 8, 3)},
-    {"name": "David Johnson", "birthday": datetime(1978, 11, 12)},
-    {"name": "Sarah Wilson", "birthday": datetime(1993, 4, 27)},
-    {"name": "Michael Brown", "birthday": datetime(1982, 7, 8)},
-    {"name": "Jessica Lee", "birthday": datetime(1995, 1, 18)},
-    {"name": "Daniel Kim", "birthday": datetime(1987, 9, 5)},
-    {"name": "Amanda Jones", "birthday": datetime(1989, 6, 30)},
-    {"name": "Matthew White", "birthday": datetime(1984, 12, 9)},
-    {"name": "Olivia Anderson", "birthday": datetime(1991, 3, 22)},
-    {"name": "Christopher Miller", "birthday": datetime(1975, 10, 14)},
-    {"name": "Sophia Martinez", "birthday": datetime(1986, 10, 15)},
-    {"name": "Joseph Taylor", "birthday": datetime(1992, 10, 17)},
-    {"name": "Elizabeth Harris", "birthday": datetime(1977, 10, 18)},
-    {"name": "Andrew Wilson", "birthday": datetime(1994, 10, 13)},
-    {"name": "Mykyta Moscalew", "birthday": datetime(2010, 2, 8)},
-]
+with open('data/users.json', 'r') as file:
+    users_data = json.load(file)
 
-get_birthdays_per_week(users)
+for user in users_data:
+    user["birthday"] = datetime.strptime(user["birthday"], "%Y-%m-%d")
+
+
+get_birthdays_per_week(users_data)
