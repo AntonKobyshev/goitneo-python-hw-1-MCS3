181837ec841140d96f69e242c1bfdcbfb798d0ae
Anton Kobyshev <antonkobyshev@gmail.com>
2023-10-15 11:46:37 +0300

    Update birthday_assistant.py

--- birthday_assistant.py
+++ birthday_assistant.py
@@ -4,7 +4,6 @@ from collections import defaultdict
 
 
 def get_birthdays_per_week(users_data):
-
     birthdays_per_week = defaultdict(list)
     days_of_week = {
         0: 'Monday', 1: 'Tuesday', 2: 'Wednesday',
@@ -27,18 +26,22 @@ def get_birthdays_per_week(users_data):
             if birthday_this_year.weekday() >= 5:
                 birthdays_per_week[days_of_week[0]].append(name)
             else:
-                birthdays_per_week[days_of_week[birthday_this_year.weekday()]
-                                   ].append(name)
+                birthdays_per_week[days_of_week[birthday_this_year.weekday()]].append(
+                    name)
 
     for day, users_list in birthdays_per_week.items():
         print(f"{day}: {', '.join(users_list)}")
 
 
-with open('data/users.json', 'r') as file:
-    users_data = json.load(file)
+def main():
+    with open('data/users.json', 'r') as file:
+        users_data = json.load(file)
 
-for user in users_data:
-    user["birthday"] = datetime.strptime(user["birthday"], "%Y-%m-%d")
+    for user in users_data:
+        user["birthday"] = datetime.strptime(user["birthday"], "%Y-%m-%d")
+
+    get_birthdays_per_week(users_data)
 
 
-get_birthdays_per_week(users_data)
+if __name__ == "__main__":
+    main()
